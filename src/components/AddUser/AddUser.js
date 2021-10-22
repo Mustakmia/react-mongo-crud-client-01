54d32609bfdfe2f46f04eacbea68ced44334798b
Mustakmia <musyafijur52@gmail.com>
2021-10-22 19:11:41 +0600

    first commit

---
+++ src/components/AddUser/AddUser.js
@@ -0,0 +1,44 @@
+import React, { useRef } from 'react';
+
+const AddUser = () => {
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const handleUser = e => {
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const newUser = { name, email };
+
+        fetch('http://localhost:5000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newUser),
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    alert('user successfully added')
+                    e.target.reset();
+                }
+                else {
+                    alert('user not added')
+                }
+            })
+
+        e.preventDefault();
+    }
+    return (
+        <div>
+            <h2>This is Add User</h2>
+
+            <form onSubmit={handleUser}>
+                <input type="text" placeholder="name" ref={nameRef} />
+                <input type="email" placeholder="email" ref={emailRef} />
+                <input type="submit" value="addUser" />
+            </form>
+        </div>
+    );
+};
+
+export default AddUser;
